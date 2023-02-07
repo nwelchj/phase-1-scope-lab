6248e8a42b6b0d39e64835cf89a2a971437286e3
Nolan <bugygamer.mc@gmail.com>
2023-02-06 20:53:11 -0700

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = "bob"
+//let = bestCustomer
+
+const leastFavoriteCustomer = "hi";
+ 
+
+function upperCaseCustomerName(){
+     customerName= "BOB"
+     
+};
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+        //function  changeLeastFavoriteCustomer(){
+
+}
+
+function changeLeastFavoriteCustomer(){
+
+    //console.log(leastFavoriteCustomer);
+     leastFavoriteCustomer = "Nolan"
+};  
+
